4b84939e04f66d8f242934af682331ab7e9a197f
Manu Soman <manu.naturep@gmail.com>
2016-10-24 13:06:49 +0530

    Add files via upload

--- src/canvas.js
+++ src/canvas.js
@@ -1,99 +1,99 @@
 "use strict";
 
 app.canvas = {
-	element 		: document.getElementById("Canvas"),
-	
-	width 			: 800,
-	height 			: 600,
-	BGC 			: "#FFF",
-	
-	selectedObjects : false,
-	objectList 		: []	// an array of IDs for every object in the canvas.
+    element         : document.getElementById("Canvas"),
+
+    width           : 800,
+    height          : 600,
+    BGC             : "#FFF",
+
+    selectedObjects : false,
+    objectList      : []	// an array of IDs for every object in the canvas.
 };
 
 
 
 (function(app) {
 	
-	app.canvas.element.setAttribute("width", app.canvas.width);
-	app.canvas.element.setAttribute("height", app.canvas.height);
-	app.canvas.element.style.backgroundColor = app.canvas.BGC;	
+    app.canvas.element.setAttribute("width", app.canvas.width);
+    app.canvas.element.setAttribute("height", app.canvas.height);
+    app.canvas.element.style.backgroundColor = app.canvas.BGC;	
 	
 	
-	app.canvas.handleEvent = function(e) {
-		e.preventDefault();
-		e.stopPropagation();
-		
-		let a = app.canvas.element.getBoundingClientRect(),
-			x = e.clientX - a.left,
-			y = e.clientY - a.top;
-		
-		switch(app.appUI.toolSelected) {
-			case "moveTool":
-				app.toolSet.moveTool.doTheJob(e.target, e.type, e.shiftKey, e.ctrlKey, [x, y]);
-				break;
-			case "polygonTool":
-				app.toolSet.polygonTool.doTheJob(e.type, [x, y]);
-				break;
-			case "lineTool":
-				app.toolSet.lineTool.doTheJob(e.type, [x, y]);
-				break;
-			default :
-				alert("No tool was selected");
-		}		
-	};
+    app.canvas.handleEvent = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let a = app.canvas.element.getBoundingClientRect(),
+            x = e.clientX - a.left,
+            y = e.clientY - a.top;
+
+        switch(app.appUI.toolSelected) {
+            case "moveTool":
+                app.toolSet.moveTool.doTheJob(e.target, e.type, e.shiftKey, e.ctrlKey, [x, y]);
+                break;
+            case "polygonTool":
+                app.toolSet.polygonTool.doTheJob(e.type, [x, y]);
+                break;
+            case "lineTool":
+                app.toolSet.lineTool.doTheJob(e.type, [x, y]);
+                break;
+            default :
+                alert("No tool was selected");
+        }		
+    };
     
     // Canvas event handlers start ***********************************************
-	
-	app.canvas.element.addEventListener("mousedown", window.app.canvas.handleEvent, false);
-	app.canvas.element.addEventListener("mouseup", window.app.canvas.handleEvent, false);
-	
-	// Canvas event handlers end ***********************************************
-	
-	app.canvas.manageObjSelection = function(a, b) {
+
+    app.canvas.element.addEventListener("mousedown", window.app.canvas.handleEvent, false);
+    app.canvas.element.addEventListener("mouseup", window.app.canvas.handleEvent, false);
+
+    // Canvas event handlers end ***********************************************
+
+    app.canvas.manageObjSelection = function(a, b) {
         // 'a' is the operation & 'b' is the object.
-		
-		if(a === "+") { // Single object selection.
-			app.canvas.manageObjSelection(false);
-			// Clears all the existing objects before a new selection is added.
-			
-			app.canvas.selectedObjects = [b];
-			
-		} else if(a === "++") { // Add object to existing selection.
-			
-			if(!app.canvas.selectedObjects) {
-				app.canvas.selectedObjects = [];
-			}
-			app.canvas.selectedObjects.push(b);
-			
-		} else if(a === "--") { // Minus object from existing selection.
-			
-			if(app.canvas.selectedObjects) {
-				let i = app.canvas.selectedObjects.indexOf(b),
-					t = app.canvas.selectedObjects.splice(i, 1);
-				t = null;
-				
-				if(app.canvas.selectedObjects.length === 0) {
-					app.canvas.manageObjSelection(false);
-				}
-			}
-			
-		} else { // If no selection.
-			
-			if(app.canvas.selectedObjects) {
-				let i, l = app.canvas.selectedObjects.length;
-				for(i = 0; i < l; i++) {
-					app.canvas.selectedObjects[i] = null;
-				}
-			}
-			app.canvas.selectedObjects = false;
-		}
-		
-		
-		app.appUI.layerPalette.selectionUpdate();
-		
-		return app.canvas.selectedObjects;
-		
-	};
+
+        if(a === "+") { // Single object selection.
+            app.canvas.manageObjSelection(false);
+            // Clears all the existing objects before a new selection is added.
+
+            app.canvas.selectedObjects = [b];
+
+        } else if(a === "++") { // Add object to existing selection.
+
+            if(!app.canvas.selectedObjects) {
+                app.canvas.selectedObjects = [];
+            }
+            app.canvas.selectedObjects.push(b);
+
+        } else if(a === "--") { // Minus object from existing selection.
+
+            if(app.canvas.selectedObjects) {
+                let i = app.canvas.selectedObjects.indexOf(b),
+                    t = app.canvas.selectedObjects.splice(i, 1);
+                t = null;
+
+                if(app.canvas.selectedObjects.length === 0) {
+                    app.canvas.manageObjSelection(false);
+                }
+            }
+
+        } else { // If no selection.
+
+            if(app.canvas.selectedObjects) {
+                let i, l = app.canvas.selectedObjects.length;
+                for(i = 0; i < l; i++) {
+                    app.canvas.selectedObjects[i] = null;
+                }
+            }
+            app.canvas.selectedObjects = false;
+        }
+
+
+        app.appUI.layerPalette.selectionUpdate();
+
+        return app.canvas.selectedObjects;
+
+    };
 	
 })(window.app);
